b76ae3eaa1d60e6e172e2e85144c4f3f525f184f
Fred Ulrich <fred.ulrich@shopify.com>
2019-05-13 11:51:02 -0400

    Adding query string logging

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -10,7 +10,7 @@ import play.api.mvc._
  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-
+  val logger: Logger = Logger("home")
   /**
    * Create an Action to render an HTML page.
    *
@@ -19,6 +19,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
+    logger.info(request.queryString.toString)
     Ok(views.html.index())
   }
 }
