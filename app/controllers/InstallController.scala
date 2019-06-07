49c4dc2913dbde6c94b293dae4cccd029653aa81
Fred Ulrich <9284621+fulrich@users.noreply.github.com>
2019-06-07 15:47:20 -0400

    Initial attempt at adding some basic caching.

--- app/controllers/InstallController.scala
+++ app/controllers/InstallController.scala
@@ -3,7 +3,7 @@ package controllers
 import javax.inject._
 import play.api.cache.SyncCacheApi
 import play.api.mvc._
-import shopify.installation.{InstallParameters, AuthorizeRedirect}
+import shopify.installation.AuthorizeRedirect
 import shopify.play.HmacAction
 import shopify.play.installation.InstallCallbackUri
 
@@ -17,13 +17,22 @@ class InstallController  @Inject()(hmacAction: HmacAction, cc: ControllerCompone
     )
 
     installRedirect match {
-      case Some(parsedRedirect) => Redirect(parsedRedirect.uri)
+      case Some(parsedAuthorizeRedirect) => cacheAndRedirect(parsedAuthorizeRedirect)
       case None => InternalServerError("Unable to process the installation request.")
     }
   }
 
+  def cacheAndRedirect(authorizeRedirect: AuthorizeRedirect): Result = {
+    cache.set(nonceKey(authorizeRedirect.shop), authorizeRedirect.nonce)
+    Redirect(authorizeRedirect.uri)
+  }
+
+  def nonceKey(shop: String): String = shop + "-nonce"
+
   def requestAccessCallback = Action { request =>
-    logger.info(cache.get[AuthorizeRedirect](request.getQueryString("shop").get).get.toString)
-    Ok("Calling back" + request.queryString)
+    val cachedNonce = request.getQueryString("shop").map(nonceKey).flatMap(cache.get[String].apply
+    val providedNonce = request.getQueryString("nonce")
+
+    Ok(s"Cached Nonce: $cachedNonce || Provided Nonce: $providedNonce")
   }
 }
