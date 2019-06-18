77a05745b640bd623dcf112ef0e3a659147b76c5
Fred Ulrich <fred.ulrich@shopify.com>
2019-06-18 15:25:02 -0400

    Adding some more logging for debugging purposes.

--- app/controllers/InstallController.scala
+++ app/controllers/InstallController.scala
@@ -27,6 +27,9 @@ class InstallController  @Inject()(
     val authorizeConfirmationUri = InstallCallbackUri(routes.InstallController.requestAccessCallback())
     val authorizeRedirect = AuthorizeRedirect(request.parameters, authorizeConfirmationUri)(configuration)
 
+    logger.info(s"Storing nonce ${authorizeRedirect.nonce}")
+    logger.info(s"Redirecting to ${authorizeRedirect.uri}")
+
     cache.set(request.parameters.shop + "-nonce", authorizeRedirect.nonce)
     Redirect(authorizeRedirect.uri)
   }
@@ -34,6 +37,7 @@ class InstallController  @Inject()(
   def requestAccessCallback() = actions.authorize.async { request =>
     val isNonceValid = request.parameters.validateNonce(cache.get[String](request.parameters.shop + "-nonce"))
 
+    logger.info("Validating Nonce")
     if (isNonceValid) requestToken(request.parameters)
     else Future.successful(InternalServerError("Could not validate the provided nonce."))
   }
